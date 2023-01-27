703ca0a8d25ead4fbb6194662bd297fbd30e5f9b
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-01-27 15:38:22 -0700

    created the Interviewer Item logic wiht styling, although selected is not working as intended

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -81,3 +82,45 @@ storiesOf("DayList", module)
   .add("Wednesday", () => (
       <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
+
+
+//////////////////////////////////////////////////////////////////////////////
+///////// InterviewerListItem
+//////////////////////////////////////////////////////////////////////////////
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
+
+
+
