84b420420ec268d66a05c57807335e89119c294c
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-02-02 16:36:02 -0700

    added the Show component passing onEdit and onDelete

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 //////////////////////////////////////////////////////////////////////////////
 ///////// Button
@@ -160,10 +161,10 @@ storiesOf("InterviewerList", module)
     />
   ));
 
+
 //////////////////////////////////////////////////////////////////////////////
 ///////// Appointment
 //////////////////////////////////////////////////////////////////////////////
-
 
 storiesOf("Appointment", module)
   .addParameters({
@@ -173,3 +174,11 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show 
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
