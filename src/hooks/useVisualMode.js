facd18bfa6ac89ba2f57968b26439e3a85dded9d
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-02-07 13:53:04 -0700

    Added unit testing to build a custom Hook to manage the visual mode of any component. updated modes as constants in the component then used Hook to transiciton foward and back

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const useVisualMode = (initial) => {
+  
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  
+
+  // Push new mode to the end of the history with set to current mode
+  const transition = ((newMode, replace = false) => {
+    
+    // If replace is true, overwrite last element in history
+    setHistory(prev => replace ? [...prev.slice(0, -1), newMode] : [...prev, newMode]);
+    
+    setMode(newMode);
+  });
+
+  // Pop last mode in History and set mode to last item after pop
+  // Prevent from reverting past initial mode
+  const back = (() => {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      
+      setHistory(prev => {
+        return [...prev].slice(0, -1);
+      });
+    }
+  });
+
+  return { mode , transition, back };
+};
