0837c95ea1c2638bad46686f399ee1595f175d97
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-02-08 16:47:23 -0700

    Created a function where user can EDIT an appointment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -18,6 +18,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
 
   // Visual mode logic , if interview prop exists mode is SHOW, else EMPTY
@@ -56,6 +57,7 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
        )}
        {mode === CREATE && (
@@ -74,6 +76,15 @@ export default function Appointment(props) {
           onConfirm={deleteAppointment}
         />
       )}
+      {mode === EDIT && (
+      <Form
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+        interviewers={props.interviewers}
+        onCancel={back}
+        onSave={save}
+      />
+    )}
     </article>
   );
 };
