e6bc91a1988c31043880cd8bea0fe583cc6023f4
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-02-08 18:01:08 -0700

    Refactored Application with Separation of Concerns make it mroe modular

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,80 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import { useApplicationData } from "hooks/useApplicationData";
 
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
 
-// GET Routes
-useEffect(() => {
-  Promise.all([
-    axios.get('/api/days'),
-    axios.get('/api/appointments'),
-    axios.get('/api/interviewers')
-    ]).then((responses) => {
-      setState(prev => 
-      ({...prev, days: responses[0].data,
-          appointments: responses[1].data, 
-          interviewers: responses[2].data}));
-    });
-  }, []);
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-
-// Update appointment state based on appointment id and interview
-const bookInterview = (id, interview) => {
-  const appointment = {
-    ...state.appointments[id],
-    interview: {...interview}
-  };
-  
-  const appointments = {     
-    ...state.appointments,
-    [id]: appointment
-  };
-
-  return axios.put(`/api/appointments/${id}`, {
-    interview: interview
-  }).then(res => {
-    setState({
-      ...state,
-      appointments,
-    });
-  });
-};
-
-
-// Set appointment interview to null in state, which based on appointment id
-const cancelInterview = id => {
-  const appointment = {
-    ...state.appointments[id],
-    interview: null
-  };
-
-  const appointments = {
-    ...state.appointments,
-    [id]: appointment
-  };
-
-// Call server API to cancel interview in appointment
-  return axios.delete(`/api/appointments/${id}`)
-  .then(res => {
-    setState({
-      ...state,
-      appointments,
-    });
-  });
-};
-
-const setDay = day => setState({ ...state, day });
 const dailyAppointments = getAppointmentsForDay(state, state.day);
 const interviewers = getInterviewersForDay(state, state.day);
   
