9bbdea2c902f6069b618900c2c945b82d85a9370
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-02-08 17:22:08 -0700

    added error handling

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -8,6 +8,7 @@ import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from './Confirm';
+import Error from "./Error";
 
 
 export default function Appointment(props) {
@@ -19,6 +20,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
 
   // Visual mode logic , if interview prop exists mode is SHOW, else EMPTY
@@ -37,14 +40,16 @@ export default function Appointment(props) {
     props
     .bookInterview(props.id, interview)
     .then(() => transition(SHOW))
+    .catch((err) => transition(ERROR_SAVE, true));
   };
 
-  function deleteAppointment() {
-    transition(DELETING);
+  function destroy() {
+    transition(DELETING, true);
 
     props
       .cancelInterview(props.id)
       .then(() => transition(EMPTY))
+      .catch((err) => transition(ERROR_DELETE, true));
   }
 
 
@@ -73,7 +78,7 @@ export default function Appointment(props) {
         <Confirm
           message="Are you sure you would like to delete?"
           onCancel={back}
-          onConfirm={deleteAppointment}
+          onConfirm={destroy}
         />
       )}
       {mode === EDIT && (
@@ -83,6 +88,18 @@ export default function Appointment(props) {
         interviewers={props.interviewers}
         onCancel={back}
         onSave={save}
+      />
+    )}
+    {mode === ERROR_SAVE && (
+        <Error
+          message="Could not save appointment"
+          onClose={back} 
+        />
+      )}
+    {mode === ERROR_DELETE && (
+      <Error
+        message="Could not cancel appointment"
+        onClose={back} 
       />
     )}
     </article>
